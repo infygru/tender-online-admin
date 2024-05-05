2ebc68aac629c2b4ee33d1222db8d884ca98075b
Gokula krishnan R <105733059+gokula-krishnan-r-dev@users.noreply.github.com>
2024-05-05 10:06:32 +0530

    Remove unused code and commented out sections

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
+"use client";
 import { Toaster } from "@/components/ui/toaster";
 import "@uploadthing/react/styles.css";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/components/layout/ThemeToggle/theme-provider";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +14,11 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/dashboard/tender");
+  }, []);
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} overflow-hidden`}>
